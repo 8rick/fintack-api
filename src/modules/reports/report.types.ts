9db9c2089b9afdfcf066b7a2e171441f2250e732
Rickelmy Barbosa da Silva <barbosarickelmy8@gmail.com>
2026-04-22 22:27:28 -0300

    feat: report types.

--- src/modules/reports/report.types.ts
+++ src/modules/reports/report.types.ts
@@ -14,3 +14,28 @@ export interface MonthlySummary {
     };
 }
 
+export interface CategoryReport {
+    total: number;
+
+    percentage: number;
+
+    count: number
+}
+
+export interface MonthlyHistoy {
+    month: string;
+
+    year: number;
+
+    income: number;
+    
+    expense: number;
+
+    balance: number;
+}
+
+export interface ReportFilters {
+    month?: number;
+    year?: number;
+}
+
